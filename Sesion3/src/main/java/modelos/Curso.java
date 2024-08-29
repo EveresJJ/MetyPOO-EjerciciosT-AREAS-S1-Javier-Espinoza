a25e2284425a5859189ce56460d34daeb7127472
javier espinoza <jjespinoza@uamv.edu.ni>
2024-08-29 17:32:15 -0600

    primer commit

---
+++ Sesion3/src/main/java/modelos/Curso.java
@@ -0,0 +1,13 @@
+package modelos;
+
+public class Curso {
+    private String nombre;
+
+    public String getNombre() {
+        return this.nombre;
+    }
+    public Curso(String nombre){
+        this.nombre = nombre;
+
+    }
+}
