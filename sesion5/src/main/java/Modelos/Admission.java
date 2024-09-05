0e371d933c44de0396ae38466509644da15b5d24
Javier Josué Espinoza Blandón <147861785+EveresJJ@users.noreply.github.com>
2024-09-05 17:15:16 -0600

    Add files via upload

---
+++ sesion5/src/main/java/Modelos/Admission.java
@@ -0,0 +1,15 @@
+package Modelos;
+
+public class Admission {
+    public Admission(){
+
+    }
+
+    /**
+     * Calcula si el estudiante tiene derecho a beca
+     * @param student
+     */
+    public void calcScholarship(Student student){
+        student.setScholarship(student.getAverage() >= 80);
+    }
+}//end Admission
